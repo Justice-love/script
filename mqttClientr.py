6b487bf7a82dbc1d5d5f703a2375ae80c9ed4fa3
Justice-love <eddyxu1213@126.com>
2018-11-14 14:11:58 +0800

    mqtt client

---
+++ mqttClientr.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+ip, port = '120.27.13.239', 8010
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+msg.payload.decode("utf-8"))
+
+client = mqtt.Client("client_id")
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set('admin', 'admin')
+client.connect(ip, port)
+publish.single("topic", "content", qos=0, hostname=ip, port=port, client_id='client_id', auth = {'username':"admin", 'password':"admin"})
+
+client.loop_forever()
+
